89ce1a44cb0fb52beefab25aa8b2c23b1f4889e4
junaid114 <junaid8597@gmail.com>
2025-05-19 15:18:55 +0500

    updated

--- app/db/database.py
+++ app/db/database.py
@@ -21,6 +21,6 @@ from sqlalchemy.orm import sessionmaker
 # Database URL configuration
 DATABASE_URL = "postgresql://commonplace_pdm3_user:oZkVyRxuB7GcFbYrzJQqCU815pwNfTSU@dpg-d01j9hadbo4c738qlva0-a.oregon-postgres.render.com/commonplace_pdm3"
 
-engine = create_engine(DATABASE_URL ,pool_size=5,max_overflow=10)
+engine = create_engine(DATABASE_URL, pool_size=5 ,max_overflow=10, pool_pre_ping=True, pool_recycle=3600)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 BASE = declarative_base()
