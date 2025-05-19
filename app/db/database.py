562b13516c29c6854303f2422e372b78c5b3b3d8
junaid114 <junaid8597@gmail.com>
2025-05-19 14:35:50 +0500

    env updated

--- app/db/database.py
+++ app/db/database.py
@@ -1,13 +1,28 @@
-from sqlalchemy import  create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+# from sqlalchemy import  create_engine
+# from sqlalchemy.orm import sessionmaker, declarative_base
+# import os
+# from dotenv import load_dotenv
+
+# load_dotenv()
+
+# DATABASE_URL=os.getenv("DATABASE_URL")
+# engine = create_engine(DATABASE_URL)
+# # SessionLocal = sessionmaker(autocommit=False, Autoflush=False,bind=engine)
+# SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+
+# BASE=declarative_base()
+
+
 import os
-from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-load_dotenv()
 
-DATABASE_URL=os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL")
+if not DATABASE_URL:
+    print("WARNING: DATABASE_URL environment variable not set. Using default SQLite database.")
+
 engine = create_engine(DATABASE_URL)
-# SessionLocal = sessionmaker(autocommit=False, Autoflush=False,bind=engine)
-SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
-
-BASE=declarative_base()
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
